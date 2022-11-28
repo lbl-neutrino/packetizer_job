eca8706f489c94c1055e6aa862fa0442916e3764
Matt Kramer <matt314159@gmail.com>
2022-11-27 21:29:56 -0800

    Add link_packetized.py

---
+++ link_packetized.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Run me from within e.g. /global/cfs/cdirs/dune/www/data/Module2
+
+import argparse
+import os
+from pathlib import Path
+import shutil
+
+import h5py
+
+
+BASEDIR = '/global/cfs/cdirs/dune/www/data/Module2'
+PACKETDIR = 'packetized'
+
+
+def is_packetized(path):
+    try:
+        f = h5py.File(path)
+        return ('packets' in f) and (len(f['packets']) > 0)
+    except:
+        return False
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('srcdir')
+    ap.add_argument('--really', action='store_true')
+    args = ap.parse_args()
+
+    for p in Path(args.srcdir).rglob('*.h5'):
+        if is_packetized(p):
+            reldir = p.parent
+            destdir = Path(BASEDIR).joinpath(PACKETDIR, reldir)
+            dest = destdir.joinpath(p.name)
+            if not dest.exists():
+                relpath = os.path.relpath(p.absolute(), destdir)
+                if args.really:
+                    destdir.mkdir(parents=True, exist_ok=True)
+                    # I can't figure out how to get a "../../blahblah"-like path
+                    # with pathlib
+                    #
+                    os.symlink(relpath, destdir)
+                else:
+                    print(f'mkdir -p {destdir}')
+                    print(f'ln -s {relpath} {dest}')
+                    print()
+
+
+if __name__ == '__main__':
+    main()
