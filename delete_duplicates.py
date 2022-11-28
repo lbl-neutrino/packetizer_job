2f742e2b45106964228e5f7b3b54bbab4760c730
Matt Kramer <matt314159@gmail.com>
2022-11-27 21:29:22 -0800

    Finish (for now) delete_duplicates.py

--- delete_duplicates.py
+++ delete_duplicates.py
@@ -3,6 +3,7 @@
 import argparse
 from collections import defaultdict
 from pathlib import Path
+import shutil
 
 
 BASEDIR = '/global/cfs/cdirs/dune/www/data/Module2'
@@ -20,6 +21,7 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('dir1', help='the one we delete from')
     ap.add_argument('dir2')
+    ap.add_argument('--really', action='store_true')
     args = ap.parse_args()
 
     d1 = hash_dir(args.dir1)
@@ -27,7 +29,7 @@ def main():
 
     for name in d1:
         if name in d2:
-            print(' '.join(map(str, d1[name])))
+            # print(' '.join(map(str, d1[name])))
 
             for p1 in d1[name]:
                 for p2 in d2[name]:
@@ -36,6 +38,17 @@ def main():
                     except:
                         print(f'WTF {p1} {p2}')
                         continue
+                    # reldir = p1.relative_to(BASEDIR)
+                    reldir = p1.parent
+                    destdir = Path(BASEDIR).joinpath(TRASHDIR, reldir)
+                    dest = destdir.joinpath(name)
+                    if args.really:
+                        destdir.mkdir(parents=True, exist_ok=True)
+                        shutil.move(p1, dest)
+                    else:
+                        print(f'mkdir -p {destdir}')
+                        print(f'mv {p1} {dest}')
+                        print()
 
 
 if __name__ == '__main__':
