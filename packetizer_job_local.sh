88e17d7ce6ea4395e7bef72742ac8cd70d0c46bc
Matt Kramer <matt314159@gmail.com>
2022-12-03 18:35:31 -0800

    Add packetizer_job_local.sh

---
+++ packetizer_job_local.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+infile=$1; shift
+
+NPROCS=${NPROCS:-16}
+
+source load.sh
+
+module load parallel
+
+sockdir=$(mktemp -d)
+zw_fan.py --input-chunksize 8 --output-chunksize 1 "$sockdir" "$infile" &
+
+# logdir=$SCRATCH/logs.packetizer/local.$(date -Iseconds)
+# mkdir -p "$logdir"
+seq "$NPROCS" | parallel -n0 ./packetizer_worker.py "$sockdir"
+
+zw_shutdown.py "$sockdir"
